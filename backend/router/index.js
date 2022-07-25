ab6bd2731cb69b2d672056bd5da50127034b29af
Flaviane Ribeiro <rnany13@gmail.com>
2022-07-25 12:38:08 -0300

    refactor: Importanto função controllers

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,65 +1,12 @@
 const  express = require('express');
 const router = express.Router();
-const db = require('../db/connection');
+const produtsController = require('../controllers/produts-controllers')
 
-router.get('/produts', (req,res) =>{
-    qr = `SELECT * FROM produts`
-    db.query(qr, (err, result) =>{
-        if (err) throw err;
-        if(result.length > 0)
-            res.send({ data: result})
-    })
-})
-
-router.get('/produtsAll', (req,res) =>{
-    qr = `SELECT * FROM produts where status = 1`
-    db.query(qr, (err, result) =>{
-        if (err) throw err;
-        if(result.length > 0)
-            res.send({ data: result})
-    })
-})
-
-router.get('/produts/:id', (req, res) =>{
-    let id = req.params.id;
-    qr = `SELECT * FROM produts where idprodut =  ${id}`
-    db.query(qr, (err, result) =>{
-        if (err) throw err;
-        res.send({data: result})
-    })
-})
-
-router.post('/produts', (req, res) =>{
-     let produt = req.body;
-    let qr = `INSERT INTO db_products.produts(name, img, description, inventory, status, value)
-     value('${produt.name}', '${produt.img}', '${produt.description}', '${produt.inventory}','${produt.status}', '${produt.value}')`;
-
-    db.query(qr, (err, result) =>{
-        if (err) throw err;
-        if(result.length > 0)
-            return res.send({message: 'Produto inserido no sistema'})
-        return res.send({message: 'Erro ao cadastrar'})
-    })     
-})
-
-router.delete('/produts/:id', (req, res) =>{
-    let id = req.params.id;
-    qr = `DELETE FROM produts WHERE idprodut =  ${id}`
-    db.query(qr, (err, result) =>{
-        if (err) throw err;
-        res.send({message: 'Produto deletado do sistema'})
-    })
-})
-
-router.put('/produts/:id', (req, res)=>{
-    let produt = req.body;
-    let id = req.params.id;
-    qr = `UPDATE produts SET name = '${produt.name}', img = '${produt.img}', description = '${produt.description}', inventory = '${produt.inventory}', status = '${produt.status}', value = '${produt.value}' WHERE (idprodut = '${id}');`
-
-    db.query(qr, (err, result) =>{
-        if (err) throw err;
-        return res.send({message: 'Produto alterado no sistema', result:result})
-    })   
-})
+router.get('/produts', produtsController.getProduts)
+router.get('/produtsAll', produtsController.getProdutAll)
+router.get('/produts/:id', produtsController.getProdutId)
+router.post('/produts', produtsController.creat)
+router.delete('/produts/:id', produtsController.delete)
+router.put('/produts/:id', produtsController.update)
 
 module.exports = router;
